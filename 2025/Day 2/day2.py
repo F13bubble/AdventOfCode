2d4ca44048a6c6abc2537622b7870c71c60f0196
F13bubble <f13-1nfo@outlook.com>
2025-12-03 11:10:29 +0100

    ADDED: solution day 2 part 2

--- 2025/Day 2/day2.py
+++ 2025/Day 2/day2.py
@@ -1,4 +1,5 @@
-summary = 0
+res1 = 0
+res2 = 0
 
 with open("2025/Day 2/day2_input.txt", "r") as file:
     inp = file.read()
@@ -6,7 +7,12 @@ with open("2025/Day 2/day2_input.txt", "r") as file:
     for ran in ranges:
         start, end = ran.split("-")
         for id in range(int(start), int(end)):
-            id = str(id).rstrip()
-            summary += int(id) if id[:int(len(id)/2)] == id[int(len(id)/2):] else 0
+            s = str(id).rstrip()
+            mid = len(s) // 2
+            if len(s) % 2 == 0 and s[:mid] == s[mid:]:
+                res1 += id
+            if any(s == s[:l] * (len(s) // l) for l in range(1, mid + 1)):
+                res2 += id
             
-print(summary)
+print("Part 1: ", res1)
+print("Part 2: ", res2)
